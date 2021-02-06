1b4c02dcaecaeabbef90f8503aa92ca4b7b7f2be
Myriam Dubois <myriam.dubois@skema.edu>
2021-02-06 15:06:12 +0100

    install admin bro and create /admin route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
+import { AdminModule } from '@admin-bro/nestjs';
+import { AdminBroOptions } from 'admin-bro';
 
+const adminBroOptions: AdminBroOptions = {
+  rootPath: '/admin',
+  resources: [],
+};
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-blog-project', {
       useNewUrlParser: true,
     }),
     BlogModule,
+    AdminModule.createAdmin({
+      adminBroOptions,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
